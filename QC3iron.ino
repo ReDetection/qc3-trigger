d223f2d64d5dcf5a3f52b29111fc83fd92bae9d6
Serg Buglakov <sbuglakov@xiag.ch>
2019-05-10 15:02:20 +0700

    put most usable voltages first, switch to qc2 style functions (more reliable)

--- QC3iron.ino
+++ QC3iron.ino
@@ -1,8 +1,9 @@
 #include "Button.h"
 #include <QC3Control.h>
 
+typedef void (*voltageFunc)();
 QC3Control quickCharge(0, 2);
-short voltages[] = {5000, 9000, 12000, 16000, 20000, 3600};
+short voltages[] = {5000, 12000, 16000, 20000, 9000, 3600};
 #define MAXVOLTAGE (20000)
 #define VOLTAGESCOUNT (6)
 char currentVoltageIndex = 0;
@@ -22,17 +23,34 @@ void refreshLed() {
   digitalWrite(LED, phase < voltages[currentVoltageIndex] / 10 ? HIGH : LOW);
 }
 
+void setQC3Voltage() {
+    short milliVolts = voltages[currentVoltageIndex];
+    quickCharge.setMilliVoltage(milliVolts);
+}
+
+void set5V() {
+  quickCharge.set5V();
+}
+
+void set9V() {
+  quickCharge.set9V();
+}
+
+void set12V() {
+  quickCharge.set12V();
+}
+
+voltageFunc functions[VOLTAGESCOUNT] = {set5V, set12V, setQC3Voltage, setQC3Voltage, set9V, setQC3Voltage};
 void setup() {
   pinMode(LED, OUTPUT);
-  quickCharge.begin();
+  quickCharge.begin(true);
 }
 
 void loop() {
   refreshLed();
   if (next.read()) {
     currentVoltageIndex = (currentVoltageIndex + 1) % VOLTAGESCOUNT;
-    short milliVolts = voltages[currentVoltageIndex];
-    quickCharge.setMilliVoltage(milliVolts);
+    functions[currentVoltageIndex]();
     zeroPhaseTime = millis();
   }
 }
